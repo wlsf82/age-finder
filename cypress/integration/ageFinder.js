9045b3a2d6a50f2b0b091877c8d4f1afcba8b6fc
Walmyr <wlsf82@gmail.com>
2021-06-17 16:20:17 +0200

    Improve test by using custom command
    No more duplications, unnecessary alias, and variables.
    
    This commit also fixes a grammar issue on a comment.

--- cypress/integration/ageFinder.js
+++ cypress/integration/ageFinder.js
@@ -2,10 +2,8 @@ describe('Age finder', () => {
   beforeEach(() => {
     const now = new Date('2021-06-16').getTime()
 
-    cy.clock(now) // Freeze date to June 16, 2021
+    cy.clock(now) // Freezes date to June 16, 2021
     cy.visit('/')
-    cy.get('[data-cy=birthdate-date-field]')
-      .as('dateField')
   })
 
   context('Plural', () => {
@@ -22,10 +20,7 @@ describe('Age finder', () => {
 
     inputs.forEach(input => {
       it(`pluralizes the found age - ${input.age}`, () => {
-        cy.get('@dateField')
-          .type(input.birthdate)
-          .should('have.value', input.birthdate)
-          .blur()
+        cy.setDate(input.birthdate)
     
         cy.contains('p', `You're ${input.age} years old`)
       })
@@ -34,12 +29,7 @@ describe('Age finder', () => {
 
   context('Singular', () => {
     it('singularizes when age is 1', () => {
-      const birthdate = '2020-06-15'
-  
-      cy.get('@dateField')
-        .type(birthdate)
-        .should('have.value', birthdate)
-        .blur()
+      cy.setDate('2020-06-15')
   
       cy.contains('p', "You're 1 year old")
     })
@@ -47,12 +37,7 @@ describe('Age finder', () => {
 
   context('Date more than one year ahead', () => {
     it('does not show paragraph', () => {
-      const birthdate = '2022-06-17'
-
-      cy.get('@dateField')
-        .type(birthdate)
-        .should('have.value', birthdate)
-        .blur()
+      cy.setDate('2022-06-17')
   
       cy.contains('p', "You're ").should('not.exist')
     })
