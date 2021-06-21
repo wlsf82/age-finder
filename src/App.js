d156a3d35b0ee514a12175441252f39e54c17cdb
Walmyr <wlsf82@gmail.com>
2021-06-21 10:26:40 +0200

    Fix app logic and remove unnecessary lib

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import moment from 'moment'
 import DatePicker from './components/DatePicker'
-import today from './today'
 import './App.css'
 
 function App() {
   const [ message, setMessage ] = React.useState()
 
   const handleInputChange = event => {
+    const isAfterToday = moment(event.target.value, 'YYYY-MM-DD').isAfter(moment())
     const age = moment().diff(
       moment(event.target.value, 'YYYY-MM-DD'), 'years'
     )
 
-    if (age < 0) {
+    if (isAfterToday) {
       setMessage()
     } else {
       age === 1 ?
@@ -26,7 +26,7 @@ function App() {
       <h1>Age finder</h1>
       <form>
         <DatePicker
-          max={ today() }
+          max={ moment().format('YYYY-MM-DD') }
           onChange={ handleInputChange }
           testSelector="birthdate"
         >
