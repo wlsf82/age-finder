222fe6d51799445a5cba8eb2fefe15aee3601274
Walmyr <wlsf82@gmail.com>
2021-07-06 01:34:13 +0200

    Allow code coverage for component testing

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,27 @@
+// Allow code coverage for component testing
+// https://github.com/cypress-io/cypress/issues/16798#issuecomment-855846332
+const injectDevServer = require('@cypress/react/plugins/react-scripts')
+const findReactScriptsWebpackConfig = require('@cypress/react/plugins/react-scripts/findReactScriptsWebpackConfig')
+const { startDevServer } = require('@cypress/webpack-dev-server')
+const codeCoverageTask = require('@cypress/code-coverage/task')
+
 module.exports = (on, config) => {
-  require('@cypress/code-coverage/task')(on, config)
+  codeCoverageTask(on, config)
 
   if (config.testingType === 'component') {
-    require('@cypress/react/plugins/react-scripts')(on, config)
+    injectDevServer(on, config)
+
+    const webpackConfig = findReactScriptsWebpackConfig(config, {
+      webpackConfigPath: 'react-scripts/config/webpack.config',
+    })
+    const rules = webpackConfig.module.rules.find((rule) => !!rule.oneOf).oneOf
+    const babelRule = rules.find((rule) => /babel-loader/.test(rule.loader))
+
+    babelRule.options.plugins.push(require.resolve('babel-plugin-istanbul'))
+
+    on('dev-server:start', (options) => {
+      return startDevServer({ options, webpackConfig });
+    })
   }
 
   return config
